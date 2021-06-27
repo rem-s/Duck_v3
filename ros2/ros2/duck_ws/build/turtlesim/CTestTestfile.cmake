23383aa03c703df757eb4c49ec6b417b14608f22
ReonNamatame <reon64.1173@gmail.com>
2021-06-27 15:35:39 +0900

    update docker-compose

--- ros2/ros2/duck_ws/build/turtlesim/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /root/duck_ws/src/ros_tutorials/turtlesim
-# Build directory: /root/duck_ws/build/turtlesim
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("turtlesim__py")
--- ros2/ros2/duck_ws/build/turtlesim/turtlesim__py/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /root/duck_ws/build/turtlesim/turtlesim__py
-# Build directory: /root/duck_ws/build/turtlesim/turtlesim__py
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
